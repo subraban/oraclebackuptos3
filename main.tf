6530b190c6f1d80b5c17e4d6815ab328eaae4198
subraban <136383571+subraban@users.noreply.github.com>
2023-07-05 22:22:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 provider "google" {
-  credentials = file("<path-to-your-credentials-file>")
+  credentials=file("creds.json")
   project     = "groovy-karma-388506"
   region      = "us-central1"
 }
 
 resource "google_storage_bucket" "my_bucket" {
   name     = "sqlservermedia"
+  location = "us-central1"
+  storage_class = "STANDARD"
 }
 
 resource "google_storage_bucket_object" "my_object" {
   name   = "DB2_20230621210001.trn"
   bucket = google_storage_bucket.my_bucket.name
-  source = "G:/LSCopy"
+  source = "G:/LSCopy/DB2_20230621210001.trn"
 }
