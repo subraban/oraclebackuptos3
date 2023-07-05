233fab8c6b1823f66a48d342a90d3edf82fbe1df
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 00:35:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,16 +13,16 @@ resource "google_storage_bucket" "my_bucket" {
 resource "google_storage_bucket_object" "my_object1" {
   name   = var.file1
   bucket = google_storage_bucket.my_bucket.name
-  source = "G:/LSCopy/DB2_20230621210001.trn"
+  source = var.s1
 }
 
 resource "google_storage_bucket_object" "my_object2" {
   name   = var.file2
   bucket = google_storage_bucket.my_bucket.name
-  source = "G:/LSCopy/DB2_20230621211500.trn"
+  source = var.s2
 }
 resource "google_storage_bucket_object" "my_object3" {
   name   = var.file3
   bucket = google_storage_bucket.my_bucket.name
-  source = "G:/LSCopy/HR_20230606061120.trn"
+  source = var.s3
 }
