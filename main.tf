f173ae204dedfe5628a6b3a3e7098e1979b11f3b
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 00:23:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name     = "sqlservermedia345"
+  name     = "fdsfwef"
   location = "us-central1"
   storage_class = "STANDARD"
 }
