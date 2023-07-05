5231fb4234748497d36570b207a0c107c26fd5e9
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 00:29:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "file1" {
 type=string
 default="DB2_20230621210001.trn"
 }
+variable "file2" {
+type=string
+default="DB2_20230621211500.trn"
+}
+variable "file3" {
+type=string
+default="HR_20230606061120.trn"
+}
