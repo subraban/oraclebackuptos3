9551cf25c858c47236c4fc9f9cc6b33548e9c7a4
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 00:35:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,15 @@ variable "file3" {
 type=string
 default="HR_20230606061120.trn"
 }
+variable "s1" {
+type=string
+default="G:/LSCopy/DB2_20230621210001.trn"
+}
+variable "s2" {
+type=string
+default="G:/LSCopy/DB2_20230621211500.trn"
+}
+variable "s3" {
+type=string
+default="G:/LSCopy/HR_20230606061120.trn"
+}
