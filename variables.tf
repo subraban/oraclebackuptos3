1bb98da883e70bb7fcf865b0451771bea6f1111a
subraban <136383571+subraban@users.noreply.github.com>
2023-07-05 23:09:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,3 +18,12 @@ variable "file4" {
   description = "The id of the machine image (AMI) to use for the server."
   default = "HR_20230606061500.trn"
 }
+
+variable "files" {
+  type = map(string)
+  default = {
+    # sourcefile = destfile
+    "G:/LSCopy/HR_20230606061500.trn" = "somefolder/HR_20230606061500.trn",
+    "G:/LSCopy/HR_20230606061120.trn"  = "somefolder/HR_20230606061120.trn"
+  }
+}
