9eda6b8b9c5b6419d23d0e60d8eaaf374ee14a38
subraban <136383571+subraban@users.noreply.github.com>
2023-07-05 22:51:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,7 @@ resource "google_storage_bucket" "my_bucket" {
 }
 
 resource "google_storage_bucket_object" "my_object" {
-  for_each = var.files
-  name     = each.value
-  source   = "${path.module}/${each.key}"
+  name   = "var.file1"
   bucket = google_storage_bucket.my_bucket.name
- 
+  source = "G:/LSCopy/DB2_20230621210001.trn"
 }
