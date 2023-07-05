aea72307a6a32d5cb53e24083d13d8132ed1753c
subraban <136383571+subraban@users.noreply.github.com>
2023-07-05 22:38:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "google_storage_bucket" "my_bucket" {
 }
 
 resource "google_storage_bucket_object" "my_object" {
-  name = "*"
+  name = each.value
   bucket = google_storage_bucket.my_bucket.name
-  source = "G:/LSCopy/*.*"
+  source = "G:/LSCopy"
 }
