9dabe9af5ce0e379c634b30dfbad85f81268e16d
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 00:31:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name     = "dsadasasd"
+  name     = "fklfmsdfs"
   location = "us-central1"
   storage_class = "STANDARD"
 }
