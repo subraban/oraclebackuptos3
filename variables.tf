d5e95dab32ed04d09f8286305cb47374c3205adb
subraban <136383571+subraban@users.noreply.github.com>
2023-07-05 22:50:05 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "file1" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+  default = "DB2_20230621210001.trn"
+}
+variable "file2" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+  default = "DB2_20230621211500.trn"
+}
+variable "file3" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+  default = "HR_20230606061120.trn"
+}
+variable "file4" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+  default = "HR_20230606061500.trn"
+}
