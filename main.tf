b5c4099cb9add56b785d1eccd23fc7d076d6ec6e
subraban <136383571+subraban@users.noreply.github.com>
2023-07-05 22:40:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,14 @@ provider "google" {
   project     = "groovy-karma-388506"
   region      = "us-central1"
 }
-
+variable "files" {
+  type = map(string)
+  default = {
+    # sourcefile = destfile
+    "G:\LSCopy\DB2_20230621211500.trn" = "somefolder/DB2_20230621211500.trn",
+    "G:\LSCopy\DB2_20230621210001.trn"  = "somefolder/DB2_20230621210001.trn"
+  }
+}
 resource "google_storage_bucket" "my_bucket" {
   name     = "sqlservermedia345"
   location = "us-central1"
@@ -11,7 +18,9 @@ resource "google_storage_bucket" "my_bucket" {
 }
 
 resource "google_storage_bucket_object" "my_object" {
-  name = each.value
+  for_each = var.files
+  name     = each.value
+  source   = "${path.module}/${each.key}"
   bucket = google_storage_bucket.my_bucket.name
-  source = "G:/LSCopy"
+ 
 }
