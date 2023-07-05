340f408dceeb623e85a8ed9fd226a2cd71905481
subraban <136383571+subraban@users.noreply.github.com>
2023-07-05 22:15:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  credentials = file("<path-to-your-credentials-file>")
+  project     = "groovy-karma-388506"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "sqlservermedia"
+}
+
+resource "google_storage_bucket_object" "my_object" {
+  name   = "DB2_20230621210001.trn"
+  bucket = google_storage_bucket.my_bucket.name
+  source = "G:\LSCopy\DB2_20230621210001.trn"
+}
