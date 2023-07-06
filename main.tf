e85b5d2da7368724699d97d6d25c9d04053321f2
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 23:08:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,43 @@ resource "google_storage_bucket_object" "my_object4" {
   bucket = google_storage_bucket.my_bucket.name
   source = var.s4
 }
+resource "google_storage_bucket_object" "my_object5" {
+  name   = var.file5
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s5
+}
+resource "google_storage_bucket_object" "my_object6" {
+  name   = var.file6
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s6
+}
+resource "google_storage_bucket_object" "my_object7" {
+  name   = var.file7
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s7
+}
+resource "google_storage_bucket_object" "my_object8" {
+  name   = var.file8
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s8
+}
+resource "google_storage_bucket_object" "my_object9" {
+  name   = var.file9
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s
+}
+resource "google_storage_bucket_object" "my_object10" {
+  name   = var.file10
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s10
+}
+resource "google_storage_bucket_object" "my_object11" {
+  name   = var.file11
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s11
+}
+resource "google_storage_bucket_object" "my_object12" {
+  name   = var.file12
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.s12
+}
