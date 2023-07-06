c5c2136cb59c1044866671721333201370df0372
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 23:06:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,97 @@
 variable "file1" {
 type=string
-default="DB2_20230621210001.trn"
+default="O1_MF_LB81PQYS_.CTL"
 }
 variable "file2" {
 type=string
-default="DB2_20230621211500.trn"
+default="O1_MF_LB81PQXT_.CTL"
 }
 variable "file3" {
 type=string
-default="HR_20230606061120.trn"
+default="O1_MF_SYSAUX_LB81JJD9_.DBF"
 }
 variable "file4" {
 type=string
-default="sqlserver2017-kb4038634-x64_a75ab79103d72ce094866404607c2e84ae777d43"
+default="O1_MF_SYSTEM_LB81HF31_.DBF"
+}
+variable "file5" {
+type=string
+default="O1_MF_UNDOTBS1_LB81JZMR_.DBF"
+}
+variable "file6" {
+type=string
+default="O1_MF_USERS_LB81K0V2_.DBF"
+}
+variable "file7" {
+type=string
+default="O1_MF_1_LB81Q0PL_.LOG"
+}
+variable "file8" {
+type=string
+default="O1_MF_2_LB81Q0R1_.LOG"
+}
+variable "file9" {
+type=string
+default="O1_MF_3_LB81Q0S0_.LOG"
+}
+variable "file10" {
+type=string
+default="O1_MF_1_LB81PX8C_.LOG"
+}
+variable "file11" {
+type=string
+default="O1_MF_2_LB81PX8T_.LOG"
+}
+variable "file12" {
+type=string
+default="O1_MF_3_LB81PX9B_.LOG"
 }
 
 variable "s1" {
 type=string
-default="G:/LSCopy/DB2_20230621210001.trn"
+default="D:/Software/fast_recovery_area/ORCL/CONTROLFILE/O1_MF_LB81PQYS_.CTL"
 }
 variable "s2" {
 type=string
-default="G:/LSCopy/DB2_20230621211500.trn"
+default="D:/SOFTWARE/ORADATA/ORCL/CONTROLFILE/O1_MF_LB81PQXT_.CTL"
 }
 variable "s3" {
 type=string
-default="G:/LSCopy/HR_20230606061120.trn"
+default="D:/SOFTWARE/ORADATA/ORCL/DATAFILE/O1_MF_SYSAUX_LB81JJD9_.DBF"
 }
 variable "s4" {
 type=string
-default="//35.208.162.58/D$/SQL2017/sqlserver2017-kb4038634-x64_a75ab79103d72ce094866404607c2e84ae777d43"
+default="D:/SOFTWARE/ORADATA/ORCL/DATAFILE/O1_MF_SYSTEM_LB81HF31_.DBF"
+}
+variable "s5" {
+type=string
+default="D:/SOFTWARE/ORADATA/ORCL/DATAFILE/O1_MF_UNDOTBS1_LB81JZMR_.DBF"
+}
+variable "s6" {
+type=string
+default="D:/SOFTWARE/ORADATA/ORCL/DATAFILE/O1_MF_USERS_LB81K0V2_.DBF"
+}
+variable "s7" {
+type=string
+default="D:/SOFTWARE/FAST_RECOVERY_AREA/ORCL/ONLINELOG/O1_MF_1_LB81Q0PL_.LOG"
+}
+variable "s8" {
+type=string
+default="D:/SOFTWARE/FAST_RECOVERY_AREA/ORCL/ONLINELOG/O1_MF_2_LB81Q0R1_.LOG"
+}
+variable "s9" {
+type=string
+default="D:/SOFTWARE/FAST_RECOVERY_AREA/ORCL/ONLINELOG/O1_MF_3_LB81Q0S0_.LOG"
+}
+variable "s10" {
+type=string
+default="D:/SOFTWARE/ORADATA/ORCL/ONLINELOG/O1_MF_1_LB81PX8C_.LOG"
+}
+variable "s11" {
+type=string
+default="D:/SOFTWARE/ORADATA/ORCL/ONLINELOG/O1_MF_2_LB81PX8T_.LOG"
+}
+variable "s12" {
+type=string
+default="D:/SOFTWARE/ORADATA/ORCL/ONLINELOG/O1_MF_3_LB81PX9B_.LOG"
 }
