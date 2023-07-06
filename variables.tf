229f238c92fc1ef0aa4df8ca44474529f4fa5072
subraban <136383571+subraban@users.noreply.github.com>
2023-07-06 22:13:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,6 +10,11 @@ variable "file3" {
 type=string
 default="HR_20230606061120.trn"
 }
+variable "file4" {
+type=string
+default="sqlserver2017-kb4038634-x64_a75ab79103d72ce094866404607c2e84ae777d43"
+}
+
 variable "s1" {
 type=string
 default="G:/LSCopy/DB2_20230621210001.trn"
@@ -21,4 +26,8 @@ default="G:/LSCopy/DB2_20230621211500.trn"
 variable "s3" {
 type=string
 default="G:/LSCopy/HR_20230606061120.trn"
+}
+variable "s4" {
+type=string
+default="\\34.31.90.17\D$\SQL2017\sqlserver2017-kb4038634-x64_a75ab79103d72ce094866404607c2e84ae777d43"
 }
